3a21c796361922967846d49150c4a395c1b15e09
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-08-31 16:39:25 +0300

    fix: Correct About

--- src/components/AboutBlock.jsx
+++ src/components/AboutBlock.jsx
@@ -1,13 +1,12 @@
 import List from './List';
 
 const summaryArray = [
-  `Higher technical education (GPA 4.2), advanced English (C1), vast experience of working in an international cross-functional team`,
-  `I've been coding for 1.5 years, after switching the career from Mechanical Design Engineering`,
-  `Started with HTML, CSS & JavaScript and later found joy in building React-based apps`,
-  `I am solid in Algorithms, have skills in Figma`,
-  `I'm eager to create eye-catching interfaces with cutting-edge web technologies`,
-  `I am focused on blending aesthetics and functionality, driven by a keen eye for detail`,
-  `My aim for the nearest future is to continuously evolve in the dynamic realm of modern Frontend Development`,
+  `1.5 years in Frontend Development, Higher technical education (GPA 4.2), advanced English (C1), collaboration in international cross-functional teams`,
+  `Technology Stack: HTML, CSS, JavaScript, React, Next, Redux, TypeScript, Tailwind`,
+  `Solid in Algorithms and DSA, have skills in Figma`,
+  `Eager to create eye-catching interfaces with cutting-edge web technologies`,
+  `Focused on blending aesthetics and functionality, driven by a keen eye for detail`,
+  `Goal for the nearest future is to continuously evolve in the realm of modern Frontend Development`,
 ];
 
 const AboutBlock = () => {
